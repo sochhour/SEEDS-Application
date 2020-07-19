1d5677818aa9607b28409f8cbf6fe55a42a2146f
Sarah Avila <sttavila@gmail.com>
2020-07-18 18:00:29 -0700

    navigation and sidebar

--- App.js
+++ App.js
@@ -1,55 +1,38 @@
-import React, {useState} from 'react';
-import { View, Text, StyleSheet, FlatList, TextInput, Alert } from 'react-native';
-import { v4 as uuidv4 } from 'uuid';
-import { uuid } from 'uuidv4';
-import Header from './components/Header';
-import AddItem from './components/AddItem';
-import ListItem from './components/ListItem';
 
-const App = () => {
-    const [items, setItems] = useState([
-        {id: uuidv4(), text: 'Milk'},
-        {id: uuidv4(), text: 'Eggs'},
-        {id: uuidv4(), text: 'Bread'},
-        {id: uuidv4(), text: 'Juice'},
-    ]);
+import React, { Component } from 'react';
+import { Platform, StyleSheet, View, Text, Button } from 'react-native';
 
-    const onChange  = (textValue) => setText(textValue);
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
-    const addItem = (text) => {
-        if (!text) {
-            Alert.alert('Error', 'Please enter an item.', 
-            [{ text: 'OK', onPress: () => console.log("OK pressed") }],
-            { cancelable: true });
-        }
-        else {
-            setItems(prevItems => {
-                return [{id: uuidv4(), text}, ...prevItems];
-            });
-        }
-    };
+import Home from './components/Home';
+import Info from "./components/Info";
 
-    const deleteItem = (id) => {
-        setItems(prevItems => {
-            return prevItems.filter(item => item.id != id);
-        });
-    };
+const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
 
-    return (
-        <View style={styles.container}>
-            <Header />
-            <AddItem addItem={addItem}/>
-            <FlatList data={items} renderItem={({item}) => (
-                <ListItem item={item} deleteItem={deleteItem}/>
-            )}/>
-        </View>
-    );
+export default class App extends Component {
+   
+    createHomeStack = () =>
+    <Stack.Navigator screenOptions={{cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS}}>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Info" component={Info} />
+    </Stack.Navigator>
+
+    render() {
+
+        return (
+            <NavigationContainer>
+                <Drawer.Navigator>
+                    <Drawer.Screen name="Home" children={this.createHomeStack}/>
+                    <Drawer.Screen name="Info" component={Info}/>
+                </Drawer.Navigator>
+            </NavigationContainer>
+        );
+    }
 };
- 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },   
-});
- 
-export default App;
+
+
+  
+
---
+++ components/Home.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Platform, StyleSheet, View, Text, Button } from 'react-native';
+ 
+
+class Home extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.headerText}>Home Screen</Text>
+                <Button title="Button Button Button" onPress={() => this.props.navigation.navigate('Info')}/>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    headerText: {
+      fontSize: 40,
+      textAlign: "center",
+      margin: 10
+    },
+  
+  });
+  
+
+export default Home;
