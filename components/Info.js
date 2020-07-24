4571602cfbed6e2990bff804c37e1a4a013130df
Sarah Avila <sttavila@gmail.com>
2020-07-23 17:26:07 -0700

    add country info list

--- components/Info.js
+++ components/Info.js
@@ -1,12 +1,57 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, View, Text, Button } from 'react-native';
+import { Platform, StyleSheet, View, Text, Button, SectionList } from 'react-native';
+
+const sections = [
+  {
+    id: 0,
+    title: 'Issues',
+    data: [
+      {id: 0, text: 'Pollution'},
+      {id: 1, text: 'Waste'},
+      {id: 2, text: 'Carbon Emissions'},
+    ]
+  },
+  {
+    id: 1,
+    title: 'Organizations',
+    data: [
+      {id: 3, text: 'Agency 1'},
+      {id: 4, text: 'Agency 2'},
+    ]
+  }
+]
+
+const extractKey = ({id}) => id
  
 
 class Info extends Component {
+  renderItem = ({item}) => {
+    return (
+      <Text style={styles.row}>
+        {item.text}
+      </Text>
+    )
+  }
+
+  renderSectionHeader = ({section}) => {
+    return (
+      <Text style={styles.header}>
+        {section.title}
+      </Text>
+    )
+  }
+
     render() {
         return (
             <View style={styles.container}>
                 <Text style={styles.headerText}>Info Screen</Text>
+                <SectionList
+                  //style={styles.container}
+                  sections={sections}
+                  renderItem={this.renderItem}
+                  renderSectionHeader={this.renderSectionHeader}
+                  keyExtractor={extractKey}
+                />
             </View>
         )
     }
@@ -23,8 +68,19 @@ const styles = StyleSheet.create({
       textAlign: "center",
       margin: 10
     },
-  
-  });
+    row: {
+      padding: 15,
+      marginBottom: 5,
+      //backgroundColor: 'skyblue',
+    },
+    header: {
+      padding: 15,
+      marginBottom: 5,
+      //backgroundColor: 'steelblue',
+      color: 'black',
+      fontWeight: 'bold',
+    },
+});
   
 
 export default Info;
