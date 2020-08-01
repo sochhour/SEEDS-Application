e674bbfd20378bae321b1851ab4fe89dd87dcdbe
Sarah Avila <sttavila@gmail.com>
2020-07-31 19:13:13 -0700

    basic layout of task page

--- components/Task.js
+++ components/Task.js
@@ -6,7 +6,14 @@ class Task extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text style={styles.headerText}>Task Screen</Text>
+                {/* <Text style={styles.headerText}>Task Screen</Text> */}
+                <Text style={styles.headerText}>Task: Let's save electricity!</Text>
+                <Text style={styles.situation}>You're about to leave your house to walk your dog after dinner. Before you go, </Text>
+                <Text style={styles.task}>let's turn off the lights!</Text>
+                <View style={styles.image}>
+                  <Text style={styles.task}> IMAGE</Text>
+                </View>
+                <Button title="Complete the task" style={styles.button}></Button>
             </View>
         )
     }
@@ -15,14 +22,35 @@ class Task extends Component {
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      justifyContent: 'center',
+      //justifyContent: 'center',
       alignItems: 'center'
     },
     headerText: {
-      fontSize: 40,
+      fontSize: 30,
       textAlign: "center",
-      margin: 10
+      marginTop: 50,
     },
+    situation: {
+      fontSize: 20,
+      marginTop: 20,
+      marginLeft: 10,
+      marginRight: 10,
+      textAlign: "center",
+    },
+    task: {
+      fontSize: 30,
+      textAlign: "center",
+      fontWeight: "bold",
+    },
+    image: {
+      width:200,
+      height: 200,
+      backgroundColor: "green",
+      marginTop: 60
+    },
+    button: {
+      marginTop: 50,
+    }
   
   });
   
